bcae824c82e0f178ed8b67b9e00629471ce4450e
cemilyener <cemilyener@yandex.com>
2025-04-23 13:56:13 +0300

    Tailwind CSS ve React yapı test edildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { useState } from 'react'
-
-
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <>
-          <h1 className=" bg-amber-700 text-3xl font-bold underline">
-              Hello world!
-          </h1>
-          <div className="flex items-stretch ...">
-              <div className="py-4">01</div>
-              <div className="py-12">02</div>
-              <div className="py-8">03</div>
-          </div>
-          <div className="w-32 h-30 bg-amber-700"> merhaba</div>
-      </>
-  )
+    return (
+        <div className="text-center p-10">
+            <h1 className="text-4xl font-bold text-blue-500">Board Editor Başarıyla Çalışıyor!</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
